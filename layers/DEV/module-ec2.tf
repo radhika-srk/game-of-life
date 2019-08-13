eb86e065aa6b0ada7a2235fbde9405188a32d2b4
veerendradevops <veerendra.toleti@SSLTUIODC.LOCAL>
2019-08-13 16:19:06 +0530

    added

--- layers/DEV/module-ec2.tf
+++ layers/DEV/module-ec2.tf
@@ -1,3 +1,4 @@
+#module.<MODULE_NAME>.<OUTPUT_NAME>
 module "ec2" {
   source = "../../modules/ec2"
   ami= "${var.ami}"
@@ -5,3 +6,17 @@ module "ec2" {
   key_name = "${var.key_name}"
   availability_zones = "${var.availability_zones}"
 }
+resource "null_resource" "test" {
+  connection {
+      host = "${element(module.ec2.*.public_ip, 0)}"
+      user        = "ubuntu"
+    private_key = "${file(vksonata.pem)}"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
+
