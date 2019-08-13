9a90687f30022f299f1e7123e5bcab85e74d5b6e
veerendradevops <veerendra.toleti@SSLTUIODC.LOCAL>
2019-08-13 14:17:51 +0530

    modified securitygroup

--- modules/ec2/securitygroups.tf
+++ modules/ec2/securitygroups.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "ALLPORTS_DEV" {
       from_port = 0
       to_port = 0
       protocol = "-1"
-      cidr_blocks = ["0.0.0./0"]
+      cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
       from_port =0
